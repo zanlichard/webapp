e83038121fa28faef6aec6156b6f339a1ce79c92
zeuslichard <lz503338451@163.com>
2021-03-28 08:15:07 +0800

    support sql log

--- application/storage/mysql.go
+++ application/storage/mysql.go
@@ -5,12 +5,26 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"time"
+	"webapp/logger"
 )
 
 var GDb *gorm.DB
 var err error
 
-//
+type SqlLogger struct {
+}
+
+func (log *SqlLogger) Print(values ...interface{}) {
+	level := values[0]
+	source := values[1]
+	if level == "sql" {
+		sql := values[3].(string)
+		logger.InfoFormat("%s--sql:%s", source, sql)
+	} else {
+		logger.InfoFormat("%+v", values)
+	}
+}
+
 func InitDB(serverAddr string, user string, pwd string, database string, maxOpen int, maxIdle int, idleTime int, debug bool) error {
 	connStr := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pwd, serverAddr, database)
 	GDb, err = gorm.Open("mysql", connStr)
@@ -21,8 +35,11 @@ func InitDB(serverAddr string, user string, pwd string, database string, maxOpen
 	GDb.DB().SetMaxIdleConns(maxIdle)
 	GDb.DB().SetConnMaxLifetime(time.Duration(idleTime) * time.Second)
 
+	sqlLog := &SqlLogger{}
+
 	if debug {
 		GDb.LogMode(true)
+		GDb.SetLogger(sqlLog)
 	}
 	return nil
 }
