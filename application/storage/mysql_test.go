48359ee81e30017c5d2a30185e815180a33f6de5
zeuslichard <lz503338451@163.com>
2021-04-07 09:41:17 +0800

    fix mysql transaction testcase

--- application/storage/mysql_test.go
+++ application/storage/mysql_test.go
@@ -21,14 +21,54 @@ type Order struct {
 	UpdateTime  time.Time `gorm:"column:update_time"`          // 更新时间
 }
 
-type Warehouse struct {
-	ProductId  int32     `gorm:"primary_key;column:product_id"` //商品ID
-	CategoryId int32     `gorm:"column:category_id"`            //品类ID
-	Stock      int32     `gorm:"column:stock"`                  //库存数量
-	CreateTime time.Time `gorm:"column:create_time"`            //创建时间
-	IsValid    int32     `gorm:"column:is_valid"`               //是否有效
-	UpdateTime time.Time `gorm:"column:update_time"`            //记录更新时间
+func (Order) TableName() string {
+	return "t_order"
 }
+
+type Warehouse struct {
+	ProductId  int32     `gorm:"primary_key;product_id"` //商品ID
+	CategoryId int32     `gorm:"category_id"`            //品类ID
+	Stock      int32     `gorm:"stock"`                  //库存数量
+	CreateTime time.Time `gorm:"create_time"`            //创建时间
+	IsValid    int32     `gorm:"is_valid"`               //是否有效
+	UpdateTime time.Time `gorm:"update_time"`            //记录更新时间
+}
+
+func (Warehouse) TableName() string {
+	return "t_warehouse"
+}
+
+/*
+
+CREATE TABLE `t_order` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `goods_id` int(11) NOT NULL COMMENT '品ID',
+  `product_id` int(11) DEFAULT NULL COMMENT '品ID',
+  `category_id` int(11) DEFAULT NULL COMMENT '品ID',
+  `user_id` int(11) NOT NULL COMMENT '没id',
+  `logistics_id` int(11) DEFAULT NULL COMMENT '鞯ズ',
+  `receiver` int(11) DEFAULT NULL COMMENT '栈',
+  `pay_ways` int(11) DEFAULT NULL COMMENT '支式',
+  `order_status` int(11) DEFAULT NULL COMMENT '状态',
+  `create_time` datetime DEFAULT NULL COMMENT '时',
+  `update_time` datetime DEFAULT NULL COMMENT '时',
+  `is_valid` int(11) DEFAULT '1' COMMENT '欠效',
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE `t_warehouse` (
+  `product_id` int(11) NOT NULL COMMENT '品ID',
+  `category_id` int(11) NOT NULL COMMENT '品ID',
+  `stock` int(11) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '时',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '时',
+  `is_valid` int(11) DEFAULT '1' COMMENT '欠效',
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+  insert into t_warehouse('product_id','category_id','stock','is_valid') values('33','2','100','1');
+  insert into t_warehouse('product_id','category_id','stock','is_valid') values('34','1','100','1');
+*/
 
 func TestMysqlTrans(t *testing.T) {
 	serverAddr := "192.168.37.131:3306"
@@ -38,21 +78,25 @@ func TestMysqlTrans(t *testing.T) {
 	maxIdle := 20
 	idleTimeout := 300
 	dataBase := "devtest"
-	err := InitDB(serverAddr, userName, passWord, dataBase, maxOpen, maxIdle, idleTimeout, true)
+	err := InitDB(serverAddr, userName, passWord, dataBase, maxOpen, maxIdle, idleTimeout, false)
 	if err != nil {
 		t.Errorf("init database failed for:%+v", err)
 		return
 	}
-
 	var productId int32 = 33
 	tx := GDb.Begin()
 	defer func() {
 		if r := recover(); r != nil {
+			t.Errorf("error :%+v", r)
 			tx.Rollback()
 		}
 	}()
-	rowsAffected := tx.Model(&Warehouse{}).Where("product_id=?", productId).Where("stock > 0").Update("stock", gorm.Expr("stock - 1")).RowsAffected
-	if rowsAffected == 0 {
+	result := tx.Model(&Warehouse{}).Where("product_id=?", productId).Where("stock > 0").Update("stock", gorm.Expr("stock - 1"))
+	rowsAffected := result.RowsAffected
+	err = result.Error
+	t.Logf("update warehouse")
+	if err != nil || rowsAffected == 0 {
+		t.Errorf("update t_warehouse failed for:%+v", err)
 		tx.Rollback()
 		return
 	}
@@ -82,6 +126,7 @@ func TestMysqlTrans(t *testing.T) {
 	}
 	err = tx.Create(&order).Error
 	if err != nil {
+		t.Errorf("place order failed for:%+v", err)
 		tx.Rollback()
 	} else {
 		tx.Commit()
